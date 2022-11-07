9bdcbdae7921f33af471f121d89267aac680ed97
Venkat <venkatb@mindfiresolutions.com>
2022-11-07 15:10:31 +0530

    user all

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def new 
+        @user = User.new
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path
+        else 
+            render :new
+        end
+    end
+    def update 
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to root_path
+        else 
+            render :edit
+        end
+    end
+    
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to root_path
+    end
+
+    private
+    # strong parameters
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do |i| 
+    User.create(name: "user #{i+1}", email:"user#{i+1}@gmail.com")
+end
