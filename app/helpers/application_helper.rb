af28cf30897c277d1ea38295f0dc74e45b4fbaea
Venkat <venkatb@mindfiresolutions.com>
2022-11-08 16:41:34 +0530

    adding helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Returns the full title on the page
+    def full_title(page_title = "")
+        base_title = "Ruby on Rails Tutorial"
+        if page_title.empty? 
+            base_title
+        else 
+            page_title + " | " + base_title
+        end
+    end
 end
