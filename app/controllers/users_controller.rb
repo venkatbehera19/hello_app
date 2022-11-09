049db2176853691dc9b5d54b91bf5f84d31742c1
Venkat <venkatb@mindfiresolutions.com>
2022-11-09 14:32:31 +0530

    added scss and redirection properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,13 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save
-            redirect_to root_path
+            redirect_to users_path
         end
     end
     def update 
         @user = User.find(params[:id])
         if @user.update(user_params)
-            redirect_to root_path
+            redirect_to users_path
         else 
             render :edit
         end
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     def destroy 
         @user = User.find(params[:id])
         @user.destroy
-        redirect_to root_path
+        redirect_to users_path
     end
 
     private
